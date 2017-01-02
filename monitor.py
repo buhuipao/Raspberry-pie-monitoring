7137bec13e5fafd978844657cc7a3025cacb24dd
chenhua <chenhua22@outlook.com>
2017-01-02 15:41:29 +0800

    增加10秒循环

--- monitor.py
+++ monitor.py
@@ -11,7 +11,6 @@ import time
 
 
 def monitor():
-    start = time.time()
     # 统计目录下的png图片
     files = [f for f in os.listdir('.') if f.find('jepg') != -1]
 
@@ -25,13 +24,15 @@ def monitor():
     # 如果两幅图指纹不同, 发送邮件和短信，否则只保留最后一张图片
     if phash.imgs(files[-2], files[-1]):
         notice.send_notice(files[-2:])
-        print(time.time()-start)
     else:
         print('Pictures are same!')
         for img in files[:-1]:
             os.remove(img)
-        print(time.time()-start)
         return
 
 if __name__ == '__main__':
-    monitor()
+    while 1:
+        start = time.time()
+        monitor()
+        used = time.time() - start
+        time.sleep(10-used) if used <= 10 else 0
